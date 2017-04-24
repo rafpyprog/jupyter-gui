470e9ab0edc02e436705dc133ef8336c01164b4d
Rafael Alves Ribeiro <rafael.aribeiro@mp.intra>
2017-04-24 11:27:09 -0300

    Improve objects implementation

--- pandasgui.py
+++ pandasgui.py
@@ -4,20 +4,47 @@ from tkinter import *
 from tkinter import filedialog
 
 
-class OpenFile():
-    def __init__(self, label):
-        self.button = widgets.Button(description=label)
+class FileLoader():
+    def __init__(self, action="getpath", mode="r"):
+        self.mode = mode
+        self.action = action
+
+        root = Tk()
+        root.withdraw()
+        root.attributes("-topmost", True)
+
+        if self.action == "getpath":
+            self.file = filedialog.askopenfilename()
+        elif self.action == "getfile":
+            self.file = filedialog.askopenfile(mode=self.mode)
+        else:
+            raise ValueError("{} is not a valid action.".format(self.action))
+
+
+class Button():
+    def __init__(self, label="", callback=None, callback_args=None):
+        self.label = label
+        self.callback = callback
+        self.callback_args = callback_args
+        self.show_button()
+        self.value = None
+
+    def show_button(self):
+        self.button = widgets.Button(description=self.label)
         display(self.button)
         self.button.on_click(self.on_click)
 
     def on_click(self, b):
-        f = FileLoader()
-        self.path = f.file_path
+        if self.callback_args is not None:
+            f = self.callback(**self.callback_args)
+        else:
+            f = self.callback()
+        self.value = f.file
 
 
-class FileLoader():
-    def __init__(self):
-        root = Tk()
-        root.withdraw()
-        root.attributes("-topmost", True)
-        self.file_path = filedialog.askopenfilename()
+class Open(Button):
+    def __init__(self, label="Open...", action="getpath", mode="r"):
+        self.label = label
+        self.show_button()
+        self.callback = FileLoader
+        self.callback_args = {"action": action, "mode": mode}
